e7c3d66a849e2cb261333da2bd5bf971e5879147
Marius Huluta <tedeus_amp@yahoo.com>
2018-05-29 15:33:36 +0300

    This from task commit

--- .vscode/run_pester_and_commit.ps1
+++ .vscode/run_pester_and_commit.ps1
@@ -1,9 +1,13 @@
 If(Get-Module -ListAvailable -Name "Pester")
 {
    $moduleroot = Split-Path -Parent $MyInvocation.MyCommand.Path
-   Write-Output "Pester is installed performing test"
+   Write-Output "Pester is installed performing test 22"
  
-    Invoke-Pester $moduleroot/../specs/*.ps1 
+    Invoke-Pester $moduleroot/../specs/*.ps1 -Show Failed,Summary
+    git add .
+    $comment = Read-Host -Prompt "Enter your comment"
+    git commit -m $comment
+    git push
 }else {
     Write-Output "INSTALLING PESTER..."
 }
--- powershell_scripts/serviceValidator.ps1
+++ powershell_scripts/serviceValidator.ps1
@@ -1,4 +1,5 @@
 function Test-Svc  {
     param([string]$svcName)
+    Write-Output "This is a test"
     return (Get-Service -Name $svcName).Status
 }
